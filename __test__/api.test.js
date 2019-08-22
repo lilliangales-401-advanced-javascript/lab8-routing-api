83ca85ff6f5d7f41ef145a186d35c7bebd73bca2
Lillian Gales <lilliangales@ymail.com>
2019-08-21 21:59:18 -0700

    all tests passing for product and category routes

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -5,7 +5,7 @@ const supergoose = require('./supergoose.js');
 const mockRequest = supergoose(server);
 
 describe('Products API', () => {
-  test('Creating a new product should return 201 and the created object', () => {
+  test('can create a project', () => {
     const testProduct = {
       name: 'Insomnia',
       description: 'An OK book',
@@ -21,9 +21,16 @@ describe('Products API', () => {
       });
   });
 
-  test('Creating a new product should return 201 and the created object', () => {
+  test('can update a project', () => {
 
     const testProduct = {
+      name: 'Insomnia',
+      description: 'An ok book',
+      price: 10,
+      category: 'books'
+    };
+
+    const testProductPut = {
       name: 'Insomnia',
       description: 'A BAD book',
       price: 10,
@@ -36,13 +43,135 @@ describe('Products API', () => {
         return response.body._id})
       .then(id => {
         return mockRequest.put(`/api/v1/products/${id}`)
-          .send(testProduct)
+          .send(testProductPut)
       })
       .then(response => {
         expect(response.status).toEqual(200);
         expect(response.body.description).toEqual('A BAD book');
       });
   });
+  test('can get a product', () => {
+
+    const testProduct = {
+      name: 'Insomnia',
+      description: 'A BAD book',
+      price: 10,
+      category: 'books'
+    };
+
+    return mockRequest.post('/api/v1/products')
+      .send(testProduct)
+      .then(response => {
+        return mockRequest.get(`/api/v1/products/${response.body._id}`)
+          .then(savedProduct => {
+            Object.keys(testProduct).forEach(key =>{
+              expect(savedProduct.body[key]).toEqual(testProduct[key]);
+            });
+          });
+      });
+  });
+
+  test('can delete a product', () => {
+
+    const testProduct = {
+      name: 'Insomnia',
+      description: 'A BAD book',
+      price: 10,
+      category: 'books'
+    };
+
+    return mockRequest.post('/api/v1/products')
+      .send(testProduct)
+      .then(response => {
+        return mockRequest.delete(`/api/v1/products/${response.body._id}`)
+          .then(savedProduct => {
+            expect(savedProduct.req.data).toEqual(undefined);
+          });
+      });
+  });
+
+});
+
+
+describe('Categories API', () => {
+  test('can create a category', () => {
+    const testProduct = {
+      name: 'Food',
+      description: 'YUM',
+    };
+
+    return mockRequest.post('/api/v1/categories')
+      .send(testProduct)
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body.name).toEqual('Food');
+      });
+  });
+
+  test('can update a category', () => {
+
+    const testProduct = {
+      name: 'Food',
+      description: 'YUM',
+    };
+
+    const testProductPut = {
+      name: 'Food',
+      description: 'YUM POST TEST',
+    };
+
+
+    return mockRequest.post('/api/v1/categories')
+      .send(testProduct)
+      .then(response => {
+        return response.body._id})
+      .then(id => {
+        return mockRequest.put(`/api/v1/categories/${id}`)
+          .send(testProductPut)
+      })
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body.description).toEqual('YUM POST TEST');
+      });
+  });
+  test('can get a category', () => {
+
+    const testProduct = {
+      name: 'Food',
+      description: 'YUM',
+    };
+
+
+    return mockRequest.post('/api/v1/categories')
+      .send(testProduct)
+      .then(response => {
+        return mockRequest.get(`/api/v1/categories/${response.body._id}`)
+          .then(savedProduct => {
+            Object.keys(testProduct).forEach(key =>{
+              expect(savedProduct.body[key]).toEqual(testProduct[key]);
+            });
+          });
+      });
+  });
+
+  test('can delete a category', () => {
+
+    const testProduct = {
+      name: 'Food',
+      description: 'YUM',
+    };
+
+
+    return mockRequest.post('/api/v1/categories')
+      .send(testProduct)
+      .then(response => {
+        return mockRequest.delete(`/api/v1/categories/${response.body._id}`)
+          .then(savedProduct => {
+            expect(savedProduct.req.data).toEqual(undefined);
+          });
+      });
+  });
+
 });
 
 
--- src/app.js
+++ src/app.js
@@ -9,12 +9,9 @@ const morgan = require('morgan');
 const errorHandler = require( './middleware/error.js');
 const notFound = require( './middleware/404.js' );
 
-// Models//
-const Products = require('../models/products/products');
-const products = new Products();
-
-const Categories = require('../models/categories/categories');
-const categories = new Categories();
+// Routes //
+const productRoutes = require('./routes/product-routes')
+const categoryRoutes = require('./routes/category-routes')
 
 // Prepare the express app
 const app = express();
@@ -27,108 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 // Routes
-app.get('/api/v1/categories', getCategories);
-app.post('/api/v1/categories', postCategories);
-app.get('/api/v1/categories/:id', getCategory);
-app.put('/api/v1/categories/:id', putCategories);
-app.delete('/api/v1/categories/:id', deleteCategories);
+app.use(categoryRoutes);
+app.use(productRoutes)
 
-app.get('/api/v1/products', getProducts);
-app.post('/api/v1/products', postProducts);
-app.get('/api/v1/products/:id', getProduct);
-app.put('/api/v1/products/:id', putProducts);
-app.delete('/api/v1/products/:id', deleteProducts);
 
 // Catchalls
 app.use(notFound);
 app.use(errorHandler);
-
-// ROUTE HANDLER FUNCTIONS
-function getCategories(request,response,next) {
-  // expects an array of object to be returned from the model
-  categories.get()
-    .then( data => {
-      const output = {
-        count: data.length,
-        results: data,
-      };
-      response.status(200).json(output);
-    })
-    .catch( next );
-}
-
-function getCategory(request,response,next) {
-  // expects an array with the one matching record from the model
-  categories.get(request.params.id)
-    .then( result => response.status(200).json(result[0]) )
-    .catch( next );
-}
-
-function postCategories(request,response,next) {
-  // expects the record that was just added to the database
-  categories.post(request.body)
-    .then( result => response.status(200).json(result[0]) )
-    .catch( next );
-}
-
-
-function putCategories(request,response,next) {
-  // expects the record that was just updated in the database
-  categories.put(request.params.id, request.body)
-    .then( result => response.status(200).json(result[0]) )
-    .catch( next );
-}
-
-function deleteCategories(request,response,next) {
-  // Expects no return value (resource was deleted)
-  categories.delete(request.params.id)
-    .then( result => response.status(200).json(result) )
-    .catch( next );
-}
-
-
-function getProducts(request,response,next) {
-  // expects an array of objects back
-  products.get()
-    .then( data => {
-      const output = {
-        count: data.length,
-        results: data,
-      };
-      response.status(200).json(output);
-    })
-    .catch( next );
-}
-
-function getProduct(request,response,next) {
-  // expects an array with one object in it
-  products.get(request.params.id)
-    .then( result => response.status(200).json(result[0]) )
-    .catch( next );
-}
-
-function postProducts(request,response,next) {
-  // expects the record that was just added to the database
-  products.post(request.body)
-    .then( result => response.status(200).json(result) )
-    .catch( next );
-}
-
-
-function putProducts(request,response,next) {
-  // expects the record that was just updated in the database
-  products.put(request.params.id, request.body)
-    .then( result => response.status(200).json(result) )
-    .catch( next );
-}
-
-function deleteProducts(request,response,next) {
-  // Expects no return value (the resource should be gone)
-  products.delete(request.params.id)
-    .then( result => response.status(200).json(result) )
-    .catch( next );
-}
-
 
 
 module.exports = {
